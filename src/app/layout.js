728fafe31ad0b248bbaa44335947b92f0be8028e
sekremersi-tech <sekremersi@gmail.com>
2026-08-22 13:00:15 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css"; // INI ADALAH KUNCI AGAR CSS/DESAINNYA MUNCUL
 export const metadata = {
   title: "Asrama Pemerintah Sumatera Barat",
   description: "Asrama Mahasiswa Merapi Singgalang dan Bundo Kanduang Yogyakarta",
+  verification: {
+    google: "PkJcYokRvowvU0LMTeBlu54SX2NQRyI_rrpcXMZRzEY",
+  },
 };
 
 export default function RootLayout({ children }) {
